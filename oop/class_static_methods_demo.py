a89e102109dc2bf89307b2273bddad6887f0f3a6
Danson45Abuya <dansonabuya700@gmail.com>
2025-08-10 23:44:24 +0300

    Class static methods

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,22 @@
+# class_static_methods_demo.py
+
+class Calculator:
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        """
+        Static method to add two numbers.
+        Does not require access to the class or instance.
+        """
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        """
+        Class method to multiply two numbers.
+        Has access to the class (cls) and can use class attributes.
+        """
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
